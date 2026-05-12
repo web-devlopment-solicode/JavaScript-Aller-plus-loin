1503f855ceec933679efae1ac70377f6093060f4
mahfoudeoumaima-ui <mahfoudeoumaima@gmail.com>
2026-05-12 11:11:36 +0200

    Add files via upload

---
+++ Chapitre 6 – Factorisation, modularisation et bonnes pratiques ES/math.js
@@ -0,0 +1,2 @@
+export const addition = (a, b) => a + b;
+export const soustraction = (a, b) => a - b;
---
+++ Chapitre 6 – Factorisation, modularisation et bonnes pratiques ES/script.js
@@ -0,0 +1,4 @@
+import { addition, soustraction } from './math.js';
+
+console.log(`5 + 3 = ${addition(5, 3)}`);
+console.log(`5 - 3 = ${soustraction(5, 3)}`);
